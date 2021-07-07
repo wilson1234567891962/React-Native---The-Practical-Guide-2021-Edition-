232dee8a1ea664a045639e4de130141cb5a01320
LIZETH <lizethgio57@gmail.com>
2021-07-07 14:19:31 -0500

    77. Working with Network (Web) Images

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -10,7 +10,11 @@ const GameOverScreen = props => {
       <TitleText>The Game is Over!</TitleText>
       <View style={styles.imageContainer}>
         <Image
-          source={require('../assets/success.png')}
+          // source={require('../assets/success.png')}
+          source={{
+            uri:
+              'https://cdn.pixabay.com/photo/2016/05/05/23/52/mountain-summit-1375015_960_720.jpg'
+          }}
           style={styles.image}
           resizeMode="cover"
         />
